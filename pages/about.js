32959b2c7f980ec8552b228b7ae890b568042a72
fullMoonAdmin <94403827+fullMoonAdmin@users.noreply.github.com>
2022-09-28 13:10:22 -0500

    Initial commit

---
+++ pages/about.js
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function About() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to About page!
+        </h1>
+
+       
+
+      </main>
+    </div>
+  )
+}
